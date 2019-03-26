5e0203cbb32f74b74c38e9a259d7136b1553eb9a
PrincetonJRose <decoyzpr@gmail.com>
2019-03-26 11:30:09 -0400

    Changes to main menu structure and create user method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ActiveRecord::Base
+    validates :name, length: { in: 6..16 }
 end
--- app/movies_tracker.rb
+++ app/movies_tracker.rb
@@ -10,48 +10,87 @@ def clear_screen
     end
 end
 
-def main_menu
+def menu_banner(menu = "Menu")
+    clear_screen
+    puts
+    puts "   Movie Tracker: #{menu}"
+    puts "======================================="
+end
+
+def login
     clear_screen
     puts
     puts "  --  Welcome to Movie Tracker!!!  --"
     puts "======================================="
-    # if User.all.size == 0
-    #     create_profile
-    # end
+    main_menu
+end
+
+def main_menu
+    menu = "main"
     puts
     puts
     puts "Are you a new or returning user?"
     puts
-    puts "* Type 1 for returning"
+    puts "* Type 1 for a returning user"
     puts "* Type 2 to create a new profile"
     puts "* Type 'Q' or 'Quit' to exit"
-    puts "* Or type 'Help' for help"
+    puts
     puts
     print "=> "
     response = gets.chomp
     response = response.downcase
-    if response == '1'
-        display_users
+    check_responses(response, menu)
+end
+
+def check_responses(response, menu)
+    if response == '1' and User.all.size != 0
+        logging_in_user
+    elsif response == '1' and User.all.size == 0
+        puts
+        puts "There are currently no users.\nYou must create a new profile.\n(Press Enter to continue)"
+        gets
+        login
     elsif response == '2'
         create_profile
     elsif response == 'q' or response == 'quit' or response == 'exit'
         exit
-    elsif response == 'h' or response == 'help'
-        display_help
     else
+        menu_banner("Main Menu")
         main_menu
     end
-end
 
 def create_profile
+    menu_banner("Profile Creation")
+    menu = "profile"
+    puts
+    puts "Create a new user profile. A name is required.\nIt must be between 6 and 16 characters long.\n(Type 'Q' to exit or 'B' to go back)"
+    puts
+    print "=> "
+    response = gets.chomp
+    check_responses(response, menu)
+    if response.length < 6 or response.length > 16
+        puts
+        puts "Error: User name must be between 6 and 16 characters long.\n(Press Enter to continue)"
+        gets
+        create_profile
+    end
+    if User.all.size == 0
+        User.create(name: response)
+    else
+        for index in 0...User.all.size
+            if User.all[index].name.downcase == response.downcase
+                puts
+                puts "Sorry, that user name is already taken. Please try again."
+                gets
+                create_profile
+            end
+        end
+        User.create(name: response)
+    end
+
+end
 
 
 
 
-
-
-
-
-
-main_menu
     
