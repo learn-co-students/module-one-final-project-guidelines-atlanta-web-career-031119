ad11732149022872cd35c43a64061f1dc4f6d53a
Ansara Hollinger <ansara.hooks@gmail.com>
2019-03-26 15:30:19 -0400

    added a few more methods

--- bin/run.rb
+++ bin/run.rb
@@ -1,13 +1,10 @@
 require_relative '../config/environment'
 
-# font = TTY::Font.new(:standard)
-# puts font.write("HELLO WORLD")
-
 # class CommandLineInterface
 
   def welcome
     font = TTY::Font.new(:standard)
-    interpolated = font.write("SCHOOL SUPPLIES") #temp name
+    interpolated = font.write("Conquer College") #temp name
 
     puts "WELCOME TO"
     sleep(2)
@@ -15,10 +12,10 @@ require_relative '../config/environment'
     sleep(3)
 
     puts "Teachers usually provide a list of unnecessary school supplies for their upcoming class."
-    sleep(1)
+    sleep(2)
 
-    puts "Here at School Supplies, students provide a realistic list of supplies you actually need."
-    sleep(1)
+    puts "Here at Conquer College, students provide a realistic list of supplies you actually need."
+    sleep(2)
 
     puts "Would you like to continue? Yes or No"
 
@@ -36,6 +33,10 @@ require_relative '../config/environment'
     puts "View a list of all courses. (1)"
     puts "View your courses. (2)"
     puts "Add a course. (3)"
+    puts "Find reviews for a course. (4)"
+    puts "Update materials for a course. (5)"
+    puts "View all materials for a grade. (6)"
+    puts "See all users in a grade. (7)"
 
     user_input = gets.chomp
     if user_input == "1"
@@ -44,8 +45,42 @@ require_relative '../config/environment'
       your_course_menu
     elsif user_input == "3"
       add_course_menu
+    elsif user_input == "4"
+      find_reviews
+    elsif user_input == "5"
+      update_materials
+    elsif user_input == "6"
+      materials_for_grade
+    elsif user_input == "7"
+      all_users_in_x_grade
     end
   end
 
+  def all_courses
+    
+  end
+
+  def your_course_menu
+
+  end
+
+  def add_course_menu
+
+  end
+
+  def find_reviews
+  end
+
+  def update_materials
+
+  end
+
+  def materials_for_grade
+
+  end
+
+  def all_users_in_x_grade
+
+  end
+
 welcome
-main_menu
