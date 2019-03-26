01016ad0d3ae00d7a5f68fff6e174e0f71ad34f8
Mouni Mullapudi <mullapudi24@gmail.com>
2019-03-26 13:31:54 -0400

    Intro on CLI

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,37 @@
 require_relative '../config/environment'
 
+# font = TTY::Font.new(:standard)
+# puts font.write("HELLO WORLD")
 
-font = TTY::Font.new(:standard)
-puts font.write("HELLO WORLD")
+# class CommandLineInterface
+
+  def welcome
+    font = TTY::Font.new(:standard)
+    interpolated = font.write("SCHOOL SUPPLIES") #temp name
+
+    puts "WELCOME TO"
+    sleep(2)
+    puts interpolated
+    sleep(3)
+
+    puts "Teachers usually provide a list of unnecessary school supplies for their upcoming class."
+    sleep(1)
+
+    puts "Here at School Supplies, students provide a realistic list of supplies you actually need."
+    sleep(1)
+
+    puts "Would you like to continue? Yes or No"
+
+    user_input = gets.chomp
+    if user_input == "Yes" || user_input == "YES" || user_input == "yes"
+      main_menu
+    else user_input == "No" || user_input == "NO" || user_input == "no"
+      puts "Goodbye!"
+    end
+  end
+
+  def main_menu
+    
+  end
+
+welcome
