81a9a6422c5123dd82ca370a54601c9b1242075c
Tyler Hueter <tylerhueter08@gmail.com>
2019-03-26 16:01:16 -0400

    minor changes

--- app/controller.rb
+++ app/controller.rb
@@ -1,16 +1,3 @@
 require 'tty-prompt'
 
 prompt = TTY::Prompt.new
-
-def welcome_message
-  puts "Welcome to TicTalk"
-  name = prompt.ask("Please enter your name to continue:")
-  if User.all include?(name: name)
-    puts "Welcome back!"
-  else
-    User.new(name.to_s)
-    puts "Nice to meet you!"
-  end
-end
-
-welcome_message
--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,5 @@
 require_relative '../config/environment'
-require_relative '../app/controller'
+#require_relative '../app/controller'
 
 
-welcome_message
+User.all
