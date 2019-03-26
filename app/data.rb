0d03e2d039f1fb55596dacf818622ff33eb24415
github username <github email address>
2019-03-26 09:57:42 -0400

    pulled data from api and saved raw data in app/raw_data.rb

---
+++ app/data.rb
@@ -0,0 +1,10 @@
+require 'rest-client'
+require 'json'
+require 'pry'
+
+
+url = "https://app.ticketmaster.com/discovery/v2/events.json?size=50&apikey=7elxdku9GGG5k8j0Xm8KWdANDgecHMV0&countryCode=US&stateCode=GA"
+response = RestClient.get(url)
+data = JSON.parse(response.body)
+
+File.write('./raw_data.rb', data)
