957e322fd0cfaae2153917d9bbe25458289b0318
Tyler Hueter <tylerhueter08@gmail.com>
2019-03-26 13:06:31 -0400

    solve genre nil issue

--- app/data.rb
+++ app/data.rb
@@ -1,15 +1,15 @@
 require 'rest-client'
 require 'json'
 require 'pry'
-require_relative './models/event'
+#require_relative './models/event'
 
 
 url = "https://app.ticketmaster.com/discovery/v2/events.json?size=50&apikey=7elxdku9GGG5k8j0Xm8KWdANDgecHMV0&countryCode=US&stateCode=GA"
 response = RestClient.get(url)
 data = JSON.parse(response.body)
 
-binding.pry
 
+# binding.pry
 File.write('./raw_data.rb', data)
 
 # File.write('./raw_data.rb', data)
@@ -21,13 +21,21 @@ File.write('./raw_data.rb', data)
 #date = data["_embedded"]["events"][0]["dates"]["start"]["localDate"]
 #start_time = data["_embedded"]["events"][0]["dates"]["start"]["localTime"]
 
+File.open('./test.rb', 'w') { |file| file.truncate(0) }
 
 data["_embedded"]["events"].each do |event|
     name = event["name"]
     location = event["_embedded"]["venues"][0]["city"]["name"]
     venue = event["_embedded"]["venues"][0]["name"]
-    genre = event["classifications"][0]["genre"]["name"]
+    if event["classifications"][0]["genre"] == nil
+      genre = "UNKNOWN"
+    else
+      genre = event["classifications"][0]["genre"]["name"]
+    end
     date = event["dates"]["start"]["localDate"]
     start_time = event["dates"]["start"]["localTime"]
-    File.append('./test.rb', "Event.create(name: #{name},location: #{location},venue: #{venue},genre: genre,date: date,start_time: start_time))
+    seed_line = "Event.create(name: \"#{name}\",location: \"#{location}\",venue: \"#{venue}\",genre: \"#{genre}\",date: \"#{date}\",start_time: \"#{start_time}\")"
+    File.open('./db/seeds.rb',"a") do |line|
+      line.puts "\r" + seed_line
+    end
 end
